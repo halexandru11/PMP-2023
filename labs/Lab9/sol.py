44a4cb169a9eeab6c158ab0865d22fddfbb3b817
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2023-12-05 19:25:29 +0200

    lab9

--- labs/Lab9/sol.py
+++ labs/Lab9/sol.py
@@ -7,48 +7,75 @@ import pymc as pm
 
 def read_data():
     df = pd.read_csv("Admission.csv")
-    admission = df["Admission"].values.astype(int)
-    gre = df["GRE"].values.astype(int)
-    gpa = df["GPA"].values.astype(float)
-
+    df = df.query("Admission == (0, 1)")
+    admission = df["Admission"].values.astype(np.int32)
+    gre = df["GRE"].values.astype(np.int32)
+    gpa = df["GPA"].values.astype(np.float32)
     return np.array(admission), np.array(gre), np.array(gpa)
 
 
-def ex1(admission, gre, gpa):
-    with pm.Model() as model:
-        beta0 = pm.Normal("beta0", mu=0, sigma=10)
-        beta1 = pm.Normal("beta1", mu=0, sigma=10)
-        beta2 = pm.Normal("beta2", mu=0, sigma=10)
+def predict(gre, gpa, idata):
+    alpha = idata.posterior["alpha"].mean(("chain", "draw"))
+    beta1 = idata.posterior["beta1"].mean(("chain", "draw"))
+    beta2 = idata.posterior["beta2"].mean(("chain", "draw"))
+    mu = alpha + gre * beta1 + gpa * beta2
+    theta = 1 / (1 + np.exp(-mu))
+    theta = np.array(theta)
+    admission_prob = az.hdi(theta, hdi_prob=0.9)
+    print(f"admission probability: {admission_prob}")
 
-        mu = beta0 + pm.math.dot(beta1, gre) + pm.math.dot(beta2, gpa)
+
+def compute():
+    admission, gre, gpa = read_data()
+
+    with pm.Model() as model:
+        alpha = pm.Normal("alpha", mu=0, sigma=20)
+        beta1 = pm.Normal("beta1", mu=0, sigma=2)
+        beta2 = pm.Normal("beta2", mu=0, sigma=2)
+
+        mu = alpha + pm.math.dot(gre, beta1) + pm.math.dot(gpa, beta2)
         theta = pm.Deterministic("theta", pm.math.sigmoid(mu))
-        bd = pm.Deterministic("bd", -beta0 / beta2 - beta1 / beta2 * gre)
+        boundary = pm.Deterministic(
+            "boundary",
+            -alpha / beta2 - beta1 / beta2 * gre,
+        )
 
         y = pm.Bernoulli("y", p=theta, observed=admission)
 
-        idata = pm.sample(1000, return_inferencedata=True)
+        idata = pm.sample(200, return_inferencedata=True)
 
-    posterior = idata.posterior.stack(sample=("chain", "draw"))
+    return admission, gre, gpa, idata
 
-    idx = np.argsort(gre)
-    bd = idata.posterior["bd"].mean(("chain", "draw"))[idx]
-    plt.scatter(gre, gpa, c=admission, cmap="bwr")
-    az.plot_hdi(gre[idx], bd, color="k")
+
+def plot_data(admission, gre, gpa, idata):
+    idx = np.argsort(gpa)
+    boundary = idata.posterior["boundary"].mean(("chain", "draw"))[idx]
+    plt.scatter(gre, gpa, c=[f"C{i}" for i in admission])
+    plt.plot(gre[idx], boundary, color="k")
+    az.plot_hdi(
+        gre,
+        idata.posterior["boundary"],
+        color="k",
+        hdi_prob=0.94,
+    )
     plt.xlabel("GRE")
     plt.ylabel("GPA")
-
-    # theta = posterior["theta"].mean("sample")
-    # idx = np.argsort(gre)
-    # plt.plot(gre[idx], theta[idx], color="C2", lw=3)
-    # plt.scatter(gre, admission, marker="o", color="C0", s=100)
-    # plt.xlabel("GRE")
-    # plt.ylabel("Admission")
-    # plt.show()
+    plt.show()
 
 
-admission, gre, gpa = read_data()
-print(admission)
-print(gre)
-print(gpa)
+def main():
+    # ex 1
+    admission, gre, gpa, idata = compute()
 
-ex1(admission, gre, gpa)
+    # ex 2
+    # plot_data(admission, gre, gpa, idata)
+
+    # ex 3
+    predict(gre=550, gpa=3.5, idata=idata)
+
+    # ex 4
+    predict(gre=500, gpa=3.2, idata=idata)
+
+
+if __name__ == "__main__":
+    main()
